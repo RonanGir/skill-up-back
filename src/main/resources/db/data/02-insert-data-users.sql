abfad1244e63e07129b64b420c370344c2987cc9
Ronan Girault <girault.ronan@gmail.com>
2020-04-20 21:56:12 +0200

    add some data

---
+++ src/main/resources/db/data/02-insert-data-users.sql
@@ -0,0 +1,2 @@
+INSERT INTO skillupdb.user (id, profile_id, firstname, lastname, username, password, email) VALUES (1, 1, 'Ronan', 'Girault', 'Ronangir', 'pwd_123456', 'ronangir@skillup.io');
+INSERT INTO skillupdb.user (id, profile_id, firstname, lastname, username, password, email) VALUES (2, 1, 'Constance', 'Charpentier', 'Conniechiwac', 'pwd_123456', 'conniechiwac@skillup.io');
