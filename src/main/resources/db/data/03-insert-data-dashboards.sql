c3563f4c915d45cc60eb4f7326033fa3ae7e5a0d
Ronan Girault <girault.ronan@gmail.com>
2020-04-21 22:51:41 +0200

    add fake data

---
+++ src/main/resources/db/data/03-insert-data-dashboards.sql
@@ -0,0 +1,2 @@
+INSERT INTO skillupdb.dashboard (id, user_id) VALUES (1, 1);
+INSERT INTO skillupdb.dashboard (id, user_id) VALUES (2, 2);
---
+++ src/main/resources/db/data/04-insert-data-categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO skillupdb.category (id, name) VALUES (1, 'BDD');
+INSERT INTO skillupdb.category (id, name) VALUES (2, 'BACK');
+INSERT INTO skillupdb.category (id, name) VALUES (3, 'FRONT');
+INSERT INTO skillupdb.category (id, name) VALUES (4, 'DEVOPS');
