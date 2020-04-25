edaa0e3b6da76180a761c79edc9600cde73bc925
Ronan Girault <girault.ronan@gmail.com>
2020-04-25 14:23:04 +0200

    add new init data & update inserts

---
+++ src/main/resources/db/data/07-insert-data-ass_dashboard_tutorial.sql
@@ -0,0 +1,21 @@
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 1);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 2);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 3);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 4);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 5);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 6);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 7);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 8);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 9);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (1, 10);
+
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 1);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 2);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 3);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 4);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 5);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 6);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 7);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 8);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 9);
+INSERT INTO skillupdb.ass_dashboard_tutorial (dashboard_id, tutorial_id) VALUES (2, 10);
