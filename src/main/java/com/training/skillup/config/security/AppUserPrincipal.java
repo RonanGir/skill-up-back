fc5e67f50f26b67ab6eaf899a2fb204bbf8ecf55
Ronan Girault <girault.ronan@gmail.com>
2020-05-04 22:28:34 +0200

    init basic auth

---
+++ src/main/java/com/training/skillup/config/security/AppUserPrincipal.java
@@ -0,0 +1,47 @@
+package com.training.skillup.config.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.training.skillup.entity.UserEntity;
+
+public class AppUserPrincipal extends UserEntity implements UserDetails {
+
+	@Autowired
+	SkillupGrantedAuthority profiles;
+
+	public AppUserPrincipal(UserEntity user) {
+		super(user);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority(this.getProfile().getName()));
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
--- src/main/java/com/training/skillup/config/security/SecurityConfig.java
+++ src/main/java/com/training/skillup/config/security/SecurityConfig.java
@@ -1,33 +1,55 @@
 package com.training.skillup.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean	
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsServiceImpl();
+	}
+
+	@Bean	
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(encoder());
+		return authProvider;
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication().withUser("user").password(encoder.encode("password")).roles("USER").and()
-				.withUser("admin").password(encoder.encode("admin")).roles("USER", "ADMIN");
+		auth.authenticationProvider(authenticationProvider());
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic().disable();
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/api/**");
+	protected void configure(HttpSecurity http) throws Exception {		
+		http
+		.csrf().disable()
+		.cors()
+		.and()
+		.authorizeRequests()
+		.antMatchers("/", "/login", "/logout").permitAll()
+		.antMatchers("/admin/api/**").hasRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		.httpBasic();
 	}
 
 }
