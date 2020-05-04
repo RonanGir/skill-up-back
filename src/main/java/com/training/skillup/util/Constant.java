3872b4e04a64c809348ca13af52e7d0f967060cb
Ronan Girault <girault.ronan@gmail.com>
2020-05-04 22:24:53 +0200

    add constanst messages

---
+++ src/main/java/com/training/skillup/util/Constant.java
@@ -0,0 +1,9 @@
+package com.training.skillup.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Constant {
+	public static final String USER_NOT_FOUND = "L'utilisateur n'éxiste pas"; 
+	public static final String WRONG_PASSWORD = "Mauvais mot de passe"; 
+}
